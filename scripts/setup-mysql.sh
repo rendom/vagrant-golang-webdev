d187b8e459dcaac27191067b9e25978717bb7c58
Rendom <a@vvt.nu>
2015-08-05 19:19:43 +0200

    wups

--- scripts/setup-mysql.sh
+++ scripts/setup-mysql.sh
@@ -5,9 +5,8 @@ MYSQL_PASS="passw0rd"
 apt-get -q -y install mariadb-server
 mysqladmin -u root password passw0rd
 
-mysql -uroot -p$MYSQL_PASS -e "CREATE USER 'dev'@'localhost' IDENTIFIED BY '$MYSQL_PASS'"
 mysql -uroot -p$MYSQL_PASS -e "CREATE DATABASE dev DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
-mysql -uroot -p$MYSQL_PASS -e "GRANT ALL PRIVILEGES ON dev . * TO 'dev'@'%';"
+mysql -uroot -p$MYSQL_PASS -e "GRANT ALL on dev.* to 'dev'@'%' IDENTIFIED BY '$MYSQL_PASS';"
 
 sed -i.bak "s/127.0.0.1/0.0.0.0/" /etc/mysql/my.cnf
 
